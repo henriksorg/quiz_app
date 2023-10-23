c61940b862a4926d7e05138cd0f988fd4fe19818
Henrik Sorg <henrik@reif-sorg.de>
2023-10-23 22:00:00 +0200

    style card

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let questions = [
     {
-        "question": "Wer hat HTML erfunden",
+        "question": "Wer hat HTML erfunden?",
         "answer_1": "Robbie Williams",
         "answer_2": "Lady Gaga",
         "answer_3": "Tim Berners-Lee",
@@ -17,6 +17,7 @@ function init() {
 }
 
 function showQuestion(){
+    document.getElementById('quiz-question').innerHTML = question["question"];
     for (let i = 1; i <= 4; i++) {
         document.getElementById("answer" + i).innerHTML = question["answer_"+ i];
     }
@@ -24,10 +25,10 @@ function showQuestion(){
 
 function answer(selection){
     if (selection == question["right_answer"]){
-        document.getElementById('answer'+selection).classList.add('backgrd_gr')
+        document.getElementById('answer'+selection).parentElement.classList.add('backgrd_gr')
     }
     else{
-        document.getElementById('answer'+selection).classList.add('backgrd_red')
+        document.getElementById('answer'+selection).parentElement.classList.add('backgrd_red')
     }
 }
 
