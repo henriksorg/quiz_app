97099ffba8593b1e923c08bf3515d23768669598
Henrik Sorg <henrik@reif-sorg.de>
2023-10-30 18:53:09 +0100

    JSON Update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let questions = [
+let questionsHtml = [
   {
-    "question": "Wer hat HTML erfunden?",
+    "question": "Who invented HTML?",
     "answer_0": "Robbie Williams",
     "answer_1": "Lady Gaga",
     "answer_2": "Tim Berners-Lee",
@@ -10,36 +10,278 @@ let questions = [
     "selected_answer": ""
   },
   {
-    "question": "CSS Frage",
-    "answer_0": "CSS Antwort",
-    "answer_1": "CSS Antwort",
-    "answer_2": "CSS Antwort",
-    "answer_3": "CSS Antwort",
-    "right_answer": 1,
+    "question": "Who is making the Web standards?",
+    "answer_0": "The World Wide Web Consortium ",
+    "answer_1": "Microsoft",
+    "answer_2": "Mozilla",
+    "answer_3": "Google",
+    "right_answer": 0,
     "answered": false,
     "selected_answer": ""
   },
   {
-    "question": "JS Frag",
-    "answer_0": "JS Antwort",
-    "answer_1": "JS Antwort",
-    "answer_2": "JS Antwort",
-    "answer_3": "JS Antwort",
+    "question": "Choose the correct HTML element for the largest heading:",
+    "answer_0": "<h1>",
+    "answer_1": "<h6>",
+    "answer_2": "<heading>",
+    "answer_3": "<head>",
+    "right_answer": 0,
+    "answered": false,
+    "selected_answer": ""
+  },
+  {
+    "question": "What is the correct HTML element for inserting a line break?",
+    "answer_0": "<lb>",
+    "answer_1": "<break>",
+    "answer_2": "<br>",
+    "answer_3": "<b>",
     "right_answer": 2,
     "answered": false,
     "selected_answer": ""
   },
   {
-    "question": "Java Frage",
-    "answer_0": "Java Frage",
-    "answer_1": "Java Frage",
-    "answer_2": "Java Frage",
-    "answer_3": "Java Frage",
+    "question": "Choose the correct HTML element to define important text",
+    "answer_0": "<important>",
+    "answer_1": "<b>",
+    "answer_2": "<i>",
+    "answer_3": "<strong>",
+    "right_answer": 3,
+    "answered": false,
+    "selected_answer": "<strong>"
+  },
+  {
+    "question": "Choose the correct HTML element to define emphasized text",
+    "answer_0": "<i>",
+    "answer_1": "<em>",
+    "answer_2": "<italic>",
+    "answer_3": "<b>",
     "right_answer": 0,
+    "answered": false,
+    "selected_answer": ""
+  },
+  {
+    "question": "Which of these elements are all <table> elements?",
+    "answer_0": "<table><head><tfoot>  ",
+    "answer_1": "<table><tr><td>  ",
+    "answer_2": "<table><tr><tt>",
+    "answer_3": "<thead><body><tr>",
+    "right_answer": 0,
+    "answered": false,
+    "selected_answer": ""
+  },
+]
+
+
+let questionsNBA = [
+  {
+    "question": "Which NBA Player has won the most championships?",
+    "answer_0": "Bill Russel",
+    "answer_1": "Lebron James",
+    "answer_2": "Michael Jordan",
+    "answer_3": "Kobe Bryant",
+    "right_answer": 0,
+    "answered": false,
+    "selected_answer": ""
+  },{
+    "question": "Who was the number 1 pick in the 2018 NBA draft?",
+    "answer_0": "Trae Young",
+    "answer_1": "Deandre Ayton",
+    "answer_2": "Luka Doncic",
+    "answer_3": "Triple J",
+    "right_answer": 0,
+    "answered": false,
+    "selected_answer": ""
+  },
+  {
+    "question": "How long is the NBA shotclock?",
+    "answer_0": "22 seconds",
+    "answer_1": "23 seconds",
+    "answer_2": "24 seconds",
+    "answer_3": "25 seconds",
+    "right_answer": 0,
+    "answered": false,
+    "selected_answer": ""
+  },
+  {
+    "question": "Which NBA MVP did Kobe Bryant Play wiith in his first 3 championship years?",
+    "answer_0": "Lebron James",
+    "answer_1": "Shaquille O Neil",
+    "answer_2": "Kobe Bryant Sr.",
+    "answer_3": "Stephen Curry",
+    "right_answer": 0,
+    "answered": false,
+    "selected_answer": ""
+  },
+  {
+    "question": "Stephen Curry, Kevin Durant, Draymond Green and ... were the 4 all stars of the Warriors in the 2017-2018 season",
+    "answer_0": "Klay Thompson",
+    "answer_1": "Josh Okogie",
+    "answer_2": "Andre Iguedala",
+    "answer_3": "Kevon Looney",
+    "right_answer": 0,
+    "answered": false,
+    "selected_answer": ""
+  },
+]
+
+
+let questionsGermany = [
+  {
+    "question": "What is the capital city of Germany",
+    "answer_0": "Berlin",
+    "answer_1": "Hamburg",
+    "answer_2": "Stuttgart",
+    "answer_3": "Munich",
+    "right_answer": 3,
+    "answered": false,
+    "selected_answer": ""
+  },
+  {
+    "question": "What is Saarland?",
+    "answer_0": "a national park",
+    "answer_1": "an amusement park",
+    "answer_2": "a federal state",
+    "answer_3": "a location from a fairytale",
+    "right_answer": 2,
+    "answered": false,
+    "selected_answer": ""
+  },
+  {
+    "question": "Where can you find the ‘Englischer Garten’?",
+    "answer_0": "Munich",
+    "answer_1": "Bremen",
+    "answer_2": "Leipzig",
+    "answer_3": "Dresden",
+    "right_answer": 0,
+    "answered": false,
+    "selected_answer": ""
+  },
+  {
+    "question": "Which of the following is Germany’s highest mountain peak?",
+    "answer_0": "Brocken",
+    "answer_1": "Zugspitze",
+    "answer_2": "Feldberg",
+    "answer_3": "Nebelhorn",
+    "right_answer": 1,
+    "answered": false,
+    "selected_answer": ""
+  },
+  {
+    "question": "In Germany, the word ‘Gymnasium’ usually refers to…",
+    "answer_0": "sporting arena",
+    "answer_1": "fitness studio",
+    "answer_2": "military training facility",
+    "answer_3": "school",
+    "right_answer": 3,
+    "answered": false,
+    "selected_answer": ""
+  },
+  {
+    "question": "In which state will you find the Black Forest?",
+    "answer_0": "Bavaria",
+    "answer_1": "Mecklenburg-Vorpommern",
+    "answer_2": "Thuringia",
+    "answer_3": "Baden-Württemberg",
+    "right_answer": 3,
+    "answered": false,
+    "selected_answer": ""
+  },
+  {
+    "question": "What daily German television program is preceded by the sound of a gong?",
+    "answer_0": "Die Sendung mit der Maus",
+    "answer_1": "Tatort",
+    "answer_2": "Tagesschau",
+    "answer_3": "Lindenstrasse",
+    "right_answer": 2,
+    "answered": false,
+    "selected_answer": ""
+  },
+  {
+    "question": "Where in Germany are you most likely to hear the greeting: Moin!",
+    "answer_0": "Frankfurt",
+    "answer_1": "Leipzig",
+    "answer_2": "Hamburg",
+    "answer_3": "Ulm",
+    "right_answer": 3,
+    "answered": false,
+    "selected_answer": ""
+  },
+  {
+    "question": "How many years did it take to complete Cologne’s cathedral? ",
+    "answer_0": "12",
+    "answer_1": "150",
+    "answer_2": "632",
+    "answer_3": "1004",
+    "right_answer": 3,
+    "answered": false,
+    "selected_answer": ""
+  },
+]
+
+
+let questionsElectronics = [
+  {
+    "question": "Electronics is the science and technology concerned with regulating flow of:",
+    "answer_0": "Heat",
+    "answer_1": "Water",
+    "answer_2": "Electricity",
+    "answer_3": "Power",
+    "right_answer": 2,
+    "answered": false,
+    "selected_answer": ""
+  },
+  {
+    "question": "Which of the following is not a physical component of an electronic circuit?",
+    "answer_0": "Capacitor",
+    "answer_1": "Inductor",
+    "answer_2": "Diode",
+    "answer_3": "Temperature",
+    "right_answer": 3,
+    "answered": false,
+    "selected_answer": ""
+  },
+  {
+    "question": "Current is measured in:",
+    "answer_0": "Ohms",
+    "answer_1": "Amperes",
+    "answer_2": "Voltage",
+    "answer_3": "Watts",
+    "right_answer": 1,
+    "answered": false,
+    "selected_answer": ""
+  },
+  {
+    "question": "If you have three 1.5V batteries in series, what would be total voltage be? ",
+    "answer_0": "3.4 V",
+    "answer_1": "1.5 V",
+    "answer_2": "4.5 V",
+    "answer_3": "3 V",
+    "right_answer": 3,
+    "answered": false,
+    "selected_answer": ""
+  },
+  {
+    "question": "An AA battery holder with 6 cells provides how many total volts?",
+    "answer_0": "1.5V",
+    "answer_1": "9V",
+    "answer_2": "6V",
+    "answer_3": "3V",
+    "right_answer": 0,
+    "answered": false,
+    "selected_answer": ""
+  },
+  {
+    "question": "Which of the following is not a property of semiconductors used in electronic devices?",
+    "answer_0": "They excite electrons",
+    "answer_1": "They do not emit light",
+    "answer_2": "They have high thermal conductivity",
+    "answer_3": "They have variable electrical conductivity",
+    "right_answer": 1,
     "answered": false,
     "selected_answer": ""
   }
 ]
+
 
 let currentQuestion = 0;
 let rightAnswers = 0;
@@ -50,13 +292,13 @@ function init() {
 }
 
 
-function renderStartPage(){
+function renderStartPage() {
   body = document.getElementById('card-body');
   body.innerHTML = renderStartPageTemplate();
 }
 
 
-function renderStartPageTemplate(){
+function renderStartPageTemplate() {
   return `
   <h2>Welcome to QuizzIt</h2>
   <h3>Which Quiz do you want to Play?</h3>
@@ -133,10 +375,10 @@ function defineQuestion() {
   let question = questions[currentQuestion];
   removeResult();
   showQuestion();
-  if(question["answered"] == true){
+  if (question["answered"] == true) {
     blockAnswers();
     enableNextQuestion();
-  }else{
+  } else {
     allowAnswers();
     disableNextQuestion();
   }
@@ -164,8 +406,8 @@ function answer(selection) {
 }
 
 
-function countRightAnswers(question, selection){
-  if (question["right_answer"] == selection){
+function countRightAnswers(question, selection) {
+  if (question["right_answer"] == selection) {
     rightAnswers++;
   }
 }
@@ -303,18 +545,18 @@ function renderResultTemplate() {
 }
 
 
-function replayQuiz(){
+function replayQuiz() {
   resetAllValues();
   renderStartPage();
 }
 
 
-function resetAllValues(){
+function resetAllValues() {
   currentQuestion = 0;
   rightAnswers = 0;
-  for(i=0; i<questions.length; i++){
+  for (i = 0; i < questions.length; i++) {
     let question = questions[i];
     question["answered"] = false;
-    question["selected_answer"]= "";
+    question["selected_answer"] = "";
   }
 }
